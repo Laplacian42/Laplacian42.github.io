29c67d0b0284c539205ad50b81ca00bbf371a4ea
Michael Wutz <Michael.Wutz1@t-online.de>
2021-03-23 00:05:09 +0100

    Initial version of Angular App with Scully support.

---
+++ scully.digitize-it.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "digitize-it",
+  outDir: './dist/static',
+  routes: {
+  }
+};
